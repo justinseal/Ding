5053415cbf892da2a202c2919562c3c17ac0289a
Justin Seal <justin@justinseal.com>
2025-06-18 12:06:49 -0500

    Initial Commit

---
+++ Ding/ContentView.swift
@@ -0,0 +1,19 @@
+// Created on 6/18/25.
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Ding/DingApp.swift
@@ -0,0 +1,12 @@
+// Created on 6/18/25.
+
+import SwiftUI
+
+@main
+struct DingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
