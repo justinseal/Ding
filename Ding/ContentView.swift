0541071f1c38f7f07e02810b03c0650e73f558ba
Justin Seal <justin@justinseal.com>
2025-06-18 14:02:49 -0500

    First Commit, and it plays a sound.

--- Ding/ContentView.swift
+++ Ding/ContentView.swift
@@ -1,16 +1,30 @@
 // Created on 6/18/25.
 
 import SwiftUI
+import AVFAudio
 
 struct ContentView: View {
+    @State private var audioPlayer: AVAudioPlayer!
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            VStack {
+                Button("Ring Me") {
+                    playSound()
+                }
+                
+            }
         }
-        .padding()
+    }
+    func playSound() {
+        let soundName = "high1"
+        guard let soundFile = NSDataAsset(name: soundName) else { return }
+        do {
+            audioPlayer = try AVAudioPlayer(data: soundFile.data)
+            audioPlayer.play()
+        } catch {
+            print("😡 Error playing sound \(error.localizedDescription)")
+        }
     }
 }
 
