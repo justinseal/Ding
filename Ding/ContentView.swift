6ad651097012607a86457155b77b42a6a32ca5d2
Justin Seal <justin@justinseal.com>
2025-06-20 13:58:41 -0500

    Added: Background photo, all sounds (generic names), Picker for sounds, option to stop if button is pressed.

--- Ding/ContentView.swift
+++ Ding/ContentView.swift
@@ -5,25 +5,56 @@ import AVFAudio
 
 struct ContentView: View {
     @State private var audioPlayer: AVAudioPlayer!
+    var sounds = ["high-short",
+                  "high",
+                  "low-bowl",
+                  "low-bowl2",
+                  "metal-mid",
+                  "mid-bowl",
+                  "mid-short",
+                  "mid-short2"]
+    @State private var selectedSound = "high"
+    @State private var isPlaying = true
     
     var body: some View {
-        ZStack {
-            VStack {
-                Button("Ring Me") {
-                    playSound()
+        VStack {
+            Image("New Lotus")
+                .resizable()
+                .scaledToFit()
+            Button {
+                playSound()
+            } label: {
+                Label("Ring me", systemImage: "bell.and.waves.left.and.right.fill")
+            }
+            .buttonStyle(.borderedProminent)
+            
+            Picker("Select a sound", selection: $selectedSound) {
+                ForEach(sounds, id: \.self) { sound in
+                    Text(sound)
                 }
-                
+                .pickerStyle(.wheel)
             }
         }
     }
+    
     func playSound() {
-        let soundName = "high1"
+        let soundName = selectedSound
         guard let soundFile = NSDataAsset(name: soundName) else { return }
-        do {
-            audioPlayer = try AVAudioPlayer(data: soundFile.data)
-            audioPlayer.play()
-        } catch {
-            print("😡 Error playing sound \(error.localizedDescription)")
+        if isPlaying {
+            do {
+                audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                isPlaying.toggle()
+                audioPlayer.play()
+            } catch {
+                print("😡 Error playing sound \(error.localizedDescription)")
+            }
+        } else {
+            do {
+                audioPlayer = try AVAudioPlayer(data: soundFile.data)
+                audioPlayer.stop()
+            } catch {
+                print("😡 Error stoping sound \(error.localizedDescription)")
+            }
         }
     }
 }
